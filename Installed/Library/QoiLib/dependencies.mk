bcd06699ddaa3a81317e6b1c504a562bfab8e58d
Konstantin Meyer <konstantin.meyer@gmail.com>
2025-08-10 12:04:24 +0200

    check in samples and new version of code

--- Installed/Library/QoiLib/dependencies.mk
+++ Installed/Library/QoiLib/dependencies.mk
@@ -1,7 +1,6 @@
 qoilib.obj \
 qoilib.eobj: extgraph.goh Objects/winC.goh Objects/metaC.goh object.goh \
-                Objects/inputC.goh Objects/uiInputC.goh Objects/visC.goh \
-                qoilib.goh
+                Objects/inputC.goh Objects/uiInputC.goh Objects/visC.goh
 qoilib.obj \
 qoilib.eobj: geos.h heap.h vm.h lmem.h file.h hugearr.h Ansi/string.h \
                 graphics.h fontID.h font.h color.h gstring.h chunkarr.h \
