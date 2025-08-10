86592333cdc358e7b2ab6acdaf439a6774896ed7
Konstantin Meyer <konstantin.meyer@gmail.com>
2025-08-10 15:20:04 +0200

    create code for export in library

--- Installed/Library/QoiLib/dependencies.mk
+++ Installed/Library/QoiLib/dependencies.mk
@@ -1,8 +1,16 @@
-qoilib.obj \
-qoilib.eobj: extgraph.goh Objects/winC.goh Objects/metaC.goh object.goh \
+qoiexp.obj \
+qoiexp.eobj: extgraph.goh Objects/winC.goh Objects/metaC.goh object.goh \
                 Objects/inputC.goh Objects/uiInputC.goh Objects/visC.goh
-qoilib.obj \
-qoilib.eobj: geos.h heap.h vm.h lmem.h file.h hugearr.h Ansi/string.h \
+qoiexp.obj \
+qoiexp.eobj: geos.h heap.h vm.h lmem.h file.h hugearr.h Ansi/string.h \
+                graphics.h fontID.h font.h color.h gstring.h chunkarr.h \
+                object.h geode.h Objects/helpCC.h input.h char.h hwr.h \
+                win.h
+qoiimp.obj \
+qoiimp.eobj: extgraph.goh Objects/winC.goh Objects/metaC.goh object.goh \
+                Objects/inputC.goh Objects/uiInputC.goh Objects/visC.goh
+qoiimp.obj \
+qoiimp.eobj: geos.h heap.h vm.h lmem.h file.h hugearr.h Ansi/string.h \
                 graphics.h fontID.h font.h color.h gstring.h chunkarr.h \
                 object.h geode.h Objects/helpCC.h input.h char.h hwr.h \
                 win.h
