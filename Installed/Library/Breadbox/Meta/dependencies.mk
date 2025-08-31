a2d1d6c4dfd8c782c2670c69687df1aa8a666760
Konstantin Meyer <konstantin.meyer@gmail.com>
2025-08-31 21:36:57 +0200

    first check in, nothing works

--- Installed/Library/Breadbox/Meta/dependencies.mk
+++ Installed/Library/Breadbox/Meta/dependencies.mk
@@ -146,35 +146,51 @@ splineop.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
                 Objects/gInterC.h Objects/Text/tCommon.h stylesh.h \
                 driver.h thread.h print.h Internal/spoolInt.h serialDr.h \
                 parallDr.h hugearr.h fileEnum.h float.h metaint.h
+svgGeom.obj \
+svgGeom.eobj: 
+svgGeom.obj \
+svgGeom.eobj: Ansi/string.h geos.h Ansi/stdlib.h Ansi/ctype.h file.h \
+                system.h geode.h graphics.h fontID.h font.h color.h \
+                gstring.h meta.h xlatLib.h vm.h lmem.h geoworks.h float.h \
+                dbglogmc.h timedate.h Ansi/stdio.h localize.h sllang.h
+svgUtil.obj \
+svgUtil.eobj: 
+svgUtil.obj \
+svgUtil.eobj: Ansi/string.h geos.h Ansi/stdlib.h Ansi/ctype.h file.h \
+                system.h geode.h graphics.h fontID.h font.h color.h \
+                gstring.h meta.h xlatLib.h vm.h lmem.h geoworks.h float.h \
+                dbglogmc.h timedate.h Ansi/stdio.h localize.h sllang.h
+svgStyle.obj \
+svgStyle.eobj: 
+svgStyle.obj \
+svgStyle.eobj: Ansi/string.h geos.h Ansi/stdlib.h Ansi/ctype.h file.h \
+                system.h geode.h graphics.h fontID.h font.h color.h \
+                gstring.h meta.h xlatLib.h vm.h lmem.h geoworks.h float.h \
+                dbglogmc.h timedate.h Ansi/stdio.h localize.h sllang.h
+svgView.obj \
+svgView.eobj: 
+svgView.obj \
+svgView.eobj: Ansi/string.h geos.h Ansi/stdlib.h Ansi/ctype.h file.h \
+                system.h geode.h graphics.h fontID.h font.h color.h \
+                gstring.h meta.h xlatLib.h vm.h lmem.h geoworks.h float.h \
+                dbglogmc.h timedate.h Ansi/stdio.h localize.h sllang.h
+svgShape.obj \
+svgShape.eobj: 
+svgShape.obj \
+svgShape.eobj: Ansi/string.h geos.h Ansi/stdlib.h Ansi/ctype.h file.h \
+                system.h geode.h graphics.h fontID.h font.h color.h \
+                gstring.h meta.h xlatLib.h vm.h lmem.h geoworks.h float.h \
+                dbglogmc.h timedate.h Ansi/stdio.h localize.h sllang.h
 svg.obj \
-svg.eobj: stdapp.goh object.goh ui.goh Objects/metaC.goh \
-                Objects/inputC.goh Objects/clipbrd.goh \
-                Objects/uiInputC.goh iacp.goh Objects/winC.goh \
-                Objects/gProcC.goh alb.goh Objects/processC.goh \
-                Objects/visC.goh Objects/vCompC.goh Objects/vCntC.goh \
-                Objects/gAppC.goh Objects/genC.goh Objects/gInterC.goh \
-                Objects/gPrimC.goh Objects/gDispC.goh Objects/gTrigC.goh \
-                Objects/gViewC.goh Objects/gTextC.goh Objects/vTextC.goh \
-                Objects/gCtrlC.goh gcnlist.goh spool.goh \
-                Objects/gFSelC.goh Objects/gGlyphC.goh \
-                Objects/gDocCtrl.goh Objects/gDocGrpC.goh \
-                Objects/gDocC.goh Objects/gContC.goh Objects/gDCtrlC.goh \
-                Objects/gEditCC.goh Objects/gBoolGC.goh \
-                Objects/gItemGC.goh Objects/gDListC.goh \
-                Objects/gItemC.goh Objects/gBoolC.goh \
-                Objects/gGadgetC.goh Objects/gToolCC.goh \
-                Objects/gValueC.goh Objects/gToolGC.goh \
-                Objects/helpCC.goh
+svg.eobj: 
 svg.obj \
-svg.eobj: geos.h heap.h geode.h resource.h ec.h object.h lmem.h \
-                graphics.h fontID.h font.h color.h gstring.h timer.h vm.h \
-                dbase.h localize.h Ansi/ctype.h timedate.h file.h \
-                sllang.h system.h geoworks.h chunkarr.h Objects/helpCC.h \
-                disk.h drive.h input.h char.h hwr.h win.h uDialog.h \
-                Objects/gInterC.h Objects/Text/tCommon.h stylesh.h \
-                driver.h thread.h print.h Internal/spoolInt.h serialDr.h \
-                parallDr.h hugearr.h fileEnum.h Ansi/string.h \
-                Ansi/stdlib.h meta.h xlatLib.h float.h dbglogmc.h \
-                Ansi/stdio.h
+svg.eobj: SVG/svg.h
+svgPath.obj \
+svgPath.eobj: 
+svgPath.obj \
+svgPath.eobj: Ansi/string.h geos.h Ansi/stdlib.h Ansi/ctype.h file.h \
+                system.h geode.h graphics.h fontID.h font.h color.h \
+                gstring.h meta.h xlatLib.h vm.h lmem.h geoworks.h float.h \
+                dbglogmc.h timedate.h Ansi/stdio.h localize.h sllang.h
 
 metaEC.geo meta.geo : geos.ldf ui.ldf ansic.ldf grobj.ldf math.ldf 
