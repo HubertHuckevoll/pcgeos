e313145903efe0b90db93a27bbea5d21e6da092a
HubertHuckevoll <HubertHuckevoll@users.noreply.github.com>
2025-09-23 12:42:03 +0200

    Move veneer sample format info to GOC resource

--- Library/Trans/Text/VeneerSample/veneerManager.asm
+++ Library/Trans/Text/VeneerSample/veneerManager.asm
@@ -11,21 +11,3 @@ TransVeneerImportOptions TransGetImportOptionsHelper
 TransVeneerExportOptions TransGetExportOptionsHelper
 
 include Library/Trans/TransCommon/transCStub.asm
-
-InfoResource    segment lmem LMEM_TYPE_GENERAL, mask LMF_IN_RESOURCE
-
-infoTable       dw      fmt_name, fmt_mask
-                D_OPTR  VeneerImportDialog
-                D_OPTR  VeneerExportDialog
-                ImpexFormatInfo <mask IFI_IMPORT_CAPABLE or \
-                                 mask IFI_EXPORT_CAPABLE>
-
-fmt_name        chunk   char
-        char    "Veneer sample",0
-fmt_name        endc
-
-fmt_mask        chunk   char
-        char    "*.txt",0
-fmt_mask        endc
-
-InfoResource    ends
