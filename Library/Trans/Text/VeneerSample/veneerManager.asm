75682a29735bc1f01ed51848f072a3c06e792374
HubertHuckevoll <HubertHuckevoll@users.noreply.github.com>
2025-09-23 13:19:53 +0200

    Expand veneer sample formats and extension metadata

--- Library/Trans/Text/VeneerSample/veneerManager.asm
+++ Library/Trans/Text/VeneerSample/veneerManager.asm
@@ -2,7 +2,9 @@ include geos.def
 include impex.def
 include Library/Trans/TransCommon/transCommonGlobal.def
 
-TransVeneerHeader InfoResource, 1, mask IDC_TEXT
+VENEER_SAMPLE_FORMAT_COUNT    equ     2
+
+TransVeneerHeader InfoResource, VENEER_SAMPLE_FORMAT_COUNT, mask IDC_TEXT
 TransVeneerImportUI      TransGetImportUIHelper
 TransVeneerExportUI      TransGetExportUIHelper
 TransVeneerInitImportUI  TransInitImportUIHelper
