c4ff2bd493f24f192af5b8c8cd1a95037bc77179
Konstantin Meyer <konstantin.meyer@gmail.com>
2025-10-10 18:19:56 +0200

    improve includes for mp3

--- Library/Audio/BSNWav/MP3/minimp3_port.c
+++ Library/Audio/BSNWav/MP3/minimp3_port.c
@@ -3,6 +3,4 @@
  * Compile as plain C with Watcom. Do not include this file elsewhere.
  */
 #define MINIMP3_IMPLEMENTATION
-#define MINIMP3_NO_SIMD
-#define MINIMP3_NO_STDIO
-#include "minimp3.h"
+#include "minimp3_conf.h"
